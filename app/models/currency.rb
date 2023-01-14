f4cac16babd9b4c75ae0ba4eb8e4fb4a449b7c39
Alex Vasse <alexandervasse@gmail.com>
2023-01-15 00:49:30 +0900

    messing around with API

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,7 @@
 class Currency < ApplicationRecord
   def current_price
+    url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=#{self.slug}"
+    request = HTTParty.get(url, headers: { "X-CMC_PRO_API_KEY" => Rails.application.credentials.dig(:pro_api_key)})
+    response = JSON.parse(request.body)
   end
 end
