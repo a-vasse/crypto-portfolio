cfa9e8e4d690304eb56c48bb74b22b7a257e49fa
Alex Vasse <alexandervasse@gmail.com>
2023-01-15 00:26:56 +0900

    added views

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,2 +1,12 @@
 class CurrenciesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+  end
+
+  def search
+  end
+
+  def calculate
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'currencies#index'
+  post 'search', to: 'currencies#search'
+  post 'calculate', to: 'currencies#calculate'
 end
