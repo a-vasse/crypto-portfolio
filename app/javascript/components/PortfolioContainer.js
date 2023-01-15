915e58d4e94c1edb3da9584600401ab4695955fb
Alex Vasse <alexandervasse@gmail.com>
2023-01-15 18:51:19 +0900

    adding search functions

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import Search from './Search';
+import Calculate from './Calculate';
+import axios from 'axios'
 
-class PortfolioContainer extends Component {
+  class PortfolioContainer extends Component {
   constructor(props) {
     super(props)
 
@@ -8,8 +11,42 @@ class PortfolioContainer extends Component {
       name: '',
       portfolio: [],
       search_results: [],
-      
+      active_currency: null,
+      amount: ''
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
+  }
+
+  handleChange(event) {
+    axios.post('http://localhost:3000/search', {
+      search: event.target.value
+    })
+
+    .then( (data) => {
+        this.setState({
+          search_results: [...data.data.currencies]
+        })
+      })
+
+    .catch ((data)=>{
+      debugger
+    })
+  }
+
+  handleSelect(event) {
+    event.preventDefault()
+    debugger
+  }
+
+  render() {
+    return(
+      <div>
+        <Search searchResults={this.state.search_results} handleSelect={this.handleSelect} handleChange={this.handleChange} />
+        <Calculate />
+      </div>
+    )
   }
 }
 
---
+++ app/javascript/components/Search.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const searchResults = this.props.searchResults.map(
+      currency => <li key={currency.id} className="currency-list-item">
+          <a href="#" className='currency' data-id={currency.id} onClick={this.props.handleSelect}><span>{currency.name}</span> - <span>{currency.currency_symbol}</span></a>
+        </li>)
+    return (
+    <div>
+      <h1>Cryptocurrency Portfolio Calculator</h1>
+      <form>
+        <div className="form-group">
+          <label>Search for a Currency:</label><br/>
+          <input onChange={this.props.handleChange} autoComplete="off" type="text" name="name" placeholder="Ex: Bitcoin, Litecoin, Ethereum..." value={this.props.name} className="field"/>
+        </div>
+        <div className="currency-list">
+          {searchResults}
+        </div>
+      </form>
+    </div>
+    );
+  }
+}
+
+export default Search;
