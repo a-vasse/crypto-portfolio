97d27298c98861e5965a296385695adf11b99274
Alex Vasse <alexandervasse@gmail.com>
2023-01-15 14:43:05 +0900

    API calls working

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,7 +1,7 @@
 class Currency < ApplicationRecord
   def current_price
-    url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=#{self.slug}"
-    request = HTTParty.get(url, headers: { "X-CMC_PRO_API_KEY" => Rails.application.credentials.dig(:pro_api_key)})
-    response = JSON.parse(request.body)
+    url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=#{self.slug}&convert=JPY"
+    request = HTTParty.get(url, headers: { "X-CMC_PRO_API_KEY" => "3fa86e6e-25bf-44bc-8ac9-b4f32ffec0bc" })
+    JSON.parse(request.body)['data'].values[0]['quote']['JPY']['price']
   end
 end
