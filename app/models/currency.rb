607caca1e1ef76fb6ee0bfa83c441a67cb622f35
Alex Vasse <alexandervasse@gmail.com>
2023-01-16 20:01:30 +0900

    changed credentials

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -5,7 +5,7 @@ class Currency < ApplicationRecord
 
   def current_price
     url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=#{self.slug}&convert=JPY"
-    request = HTTParty.get(url, headers: { "X-CMC_PRO_API_KEY" => "3fa86e6e-25bf-44bc-8ac9-b4f32ffec0bc" })
+    request = HTTParty.get(url, headers: { "X-CMC_PRO_API_KEY" => Rails.application.credentials[:coinAPI] })
     JSON.parse(request.body)['data'].values[0]['quote']['JPY']['price']
   end
 end
