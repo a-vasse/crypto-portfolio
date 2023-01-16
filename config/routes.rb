c1c030e13bdddaacafa11092374251567fb552c5
Alex Vasse <alexandervasse@gmail.com>
2023-01-17 00:26:27 +0900

    re-routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'currencies#index'
+
   post 'search', to: 'currencies#search'
   post 'calculate', to: 'currencies#calculate'
+  get '/users/:id', to: 'currencies#index', as: 'currencies'
+
+  resources :users do
+    resources :currencies
+  end
 end
